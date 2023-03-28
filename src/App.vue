6aac0e4354590a796904848536879d23c8cbedb9
Lee_XianRong <xian.rong1124@gmail.com>
2023-03-28 18:59:19 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,7 @@ export default {
           return Promise.reject(error)
       } else if(error.response.status === 400){
           const data = { 'userId': store.state.userId }
-          await axios.post(process.env.VUE_APP_DEPLOY_URL + 'refresh.php', data, {
+          await axios.post('refresh.php', data, {
             headers:{
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
